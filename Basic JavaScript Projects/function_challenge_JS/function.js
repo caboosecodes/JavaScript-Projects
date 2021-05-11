cf79f79861dd92940cf43d4d611032e9e4f8775c
caboosecodes <ricardolopez413@hotmail.com>
2021-05-11 09:07:00 -0700

    create function challenge html and js

---
+++ Basic JavaScript Projects/function_challenge_JS/function.js
@@ -0,0 +1,7 @@
+function multiply(x, y) {
+    return x * y;
+}
+
+document.getElementById("demo").innerHTML = document.write(multiply(12, 3)); 
+ // in order for me to get the above code to run WITHOUT placing the script element -
+ //after the element with the ID is by writing "document.write()" method before the function .
