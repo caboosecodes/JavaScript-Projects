b8c79bd4d838cb29a9bd18521546533f7faaf285
caboosecodes <ricardolopez413@hotmail.com>
2021-05-11 06:28:20 -0700

    create event challenge

---
+++ Basic JavaScript Projects/event_challenge_JS/event_challenge.js
@@ -0,0 +1,3 @@
+function simon() {
+    document.getElementById("garfunkel").innerHTML = "Hello darkness my old friend"; //defines fuction simon, ids the function garfunkel and will display "hello darkness my old friend"
+}
