74b9817489c7b39ddcd3e2a61f1a58ea1c3282d4
caboosecodes <ricardolopez413@hotmail.com>
2021-05-11 06:45:45 -0700

    create index and main js in project 2 folder

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function simonGarfunkel() {
+    var lyric = "Hello darkness my old friend";
+    var colorLyric = lyric.fontcolor("red");
+    document.getElementById("talk").innerHTML = colorLyric;
+}
