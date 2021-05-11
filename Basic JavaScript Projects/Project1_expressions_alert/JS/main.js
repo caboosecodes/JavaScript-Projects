d812cf0d19667b9f0b6bed917b66cc4118cb2f84
caboosecodes <ricardolopez413@hotmail.com>
2021-05-11 05:59:45 -0700

    add comments

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,23 +1,29 @@
-window.alert("Hello, World!");
+window.alert("Hello, World!"); //this pops up an alert box when you first load the page
 
-document.write("Hello, World!");
+document.write("Hello, World!"); //This writes "Hello,World" on the
 
-var greeting = "\"Hello there\" said the giant";
+var greeting = "\"Hello there\" said the giant"; // the "\" before the quotation marks in the string ensure that they are printed
 
-document.write(greeting);
+document.write(greeting); //prints the variable greeting
 
-document.write("concatenated" + " string.")
+document.write("concatenated" + " string.") //the method prints a concatenated string within the element
 
-var con = "This is the first part of the string."
+var con = "This is the first part of the string." //variable with a concatenated string
 + " This part is \"CONCATENATED\" onto previous"
 
-document.write(con);
+document.write(con); //prints the variable
 
-var mom = "Mary", dad="John", son="Jack", daughter="June";
+var mom = "Mary", dad="John", son="Jack", daughter="June"; // assigns multiple variables
 
-document.write(daughter);
+document.write(daughter); // prints one of the variables
 
-mom = mom.fontcolor(red);
-dad = dad.fontcolor(blue);
-son = son.fontcolor(green);
-daughter = daughter.fontcolor(purple);
+mom = mom.fontcolor("red"); //assigns colors to the variable
+dad = dad.fontcolor("blue");
+son = son.fontcolor("green");
+daughter = daughter.fontcolor("purple");
+
+document.write(mom + dad + son + daughter); //prints the variables with their assigned colors
+
+var expression = "This is an expression"; //example of an expression
+
+5 + 5; //another example of an expression
