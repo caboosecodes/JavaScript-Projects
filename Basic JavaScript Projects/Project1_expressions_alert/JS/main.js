aec2ba57806c8e0442939f3a11edc4b8de6c29fa
caboosecodes <ricardolopez413@hotmail.com>
2021-05-11 05:25:13 -0700

    add variables

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,6 +2,22 @@ window.alert("Hello, World!");
 
 document.write("Hello, World!");
 
-var greeting= "\"Hello there\" said the giant";
+var greeting = "\"Hello there\" said the giant";
 
 document.write(greeting);
+
+document.write("concatenated" + " string.")
+
+var con = "This is the first part of the string."
++ " This part is \"CONCATENATED\" onto previous"
+
+document.write(con);
+
+var mom = "Mary", dad="John", son="Jack", daughter="June";
+
+document.write(daughter);
+
+mom = mom.fontcolor(red);
+dad = dad.fontcolor(blue);
+son = son.fontcolor(green);
+daughter = daughter.fontcolor(purple);
