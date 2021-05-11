c8ed206a9c24fe95a66c813131fc0c05400fe16f
caboosecodes <ricardolopez413@hotmail.com>
2021-05-11 07:20:54 -0700

    add new function and comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,11 @@
 function simonGarfunkel() {
     var lyric = "Hello darkness my old friend";
     var colorLyric = lyric.fontcolor("red");
-    document.getElementById("talk").innerHTML = colorLyric;
+    document.getElementById("talk").innerHTML = colorLyric; //function with two variables, fontcolor method, and "talk" id 
+}
+
+function theBeatles()   {
+    var sun = "Here comes the sun,"
+    sun += " doo da doo doo"
+    document.getElementById("lyric").innerHTML = sun; //function with a concatenated variable, "lyric" id
 }
