5ba44e695a659539c8630972df2eaf4ecde57be5
caboosecodes <ricardolopez413@hotmail.com>
2021-05-12 09:14:59 -0700

    create main js

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,47 @@
+document.write(typeof(5));
+document.write("<br>");
+document.write(typeof("hello"));
+document.write("<br>");
+document.write("100" + 6);
+document.write("<br>");
+document.write(63E500);
+document.write("<br>");
+document.write(-32E400);
+document.write("<br>");
+document.write(6 >= 6);
+document.write("<br>");
+document.write(5 > 6);
+document.write("<br>");
+console.log(5*5);
+document.write("<br>");
+console.log(10 < 5);
+document.write(15/5 == 3);
+document.write("<br>");
+document.write(20 == 2*12);
+document.write("<br>");
+a = 10
+b = 10
+c = 15
+d = "larry"
+e = "dale"
+f = "10"
+g = 5
+document.write(a === b);
+document.write("<br>");
+document.write(a === d);
+document.write("<br>");
+document.write(a === f);
+document.write("<br>");
+document.write(d === e);
+document.write("<br>");
+document.write(c > a && a > g);
+document.write("<br>");
+document.write(a > c && a > g);
+document.write("<br>");
+document.write(a > c || a > g);
+document.write("<br>");
+document.write(a > c || a < g);
+document.write("<br>");
+document.write(!(5 > 3));
+document.write("<br>");
+document.write(!(3 > 5));
