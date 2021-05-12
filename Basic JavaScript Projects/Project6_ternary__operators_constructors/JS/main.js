a49d10c31570cb007801647afbf263221a3e89ed
caboosecodes <ricardolopez413@hotmail.com>
2021-05-12 11:18:17 -0700

    assign keyword

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -11,3 +11,17 @@ function voteFunction() {
     canVote = (age >= 18) ? "You are old enough":"You are not old enough"; // tenary operators
     document.getElementById("vote").innerHTML = canVote + " to vote."
 }
+
+function computer(cpu, gpu, mobo) {
+    this.computer_cpu = cpu;
+    this.computer_gpu = gpu;
+    this.computer_mobo = mobo;
+}
+
+var Chad = new computer("AMD Ryzen 9 5950X", "Asus 3090 TUF", "Asus ROG Crosshair VIII Hero");
+
+function myFunction()   {
+    document.getElementById("new_and_this").innerHTML = "Chad has a gaming pc with a "
+    + Chad.computer_cpu + " cpu, " + Chad.computer_gpu + " gpu, and a " + 
+    Chad.computer_mobo + " motherboard.";
+}
