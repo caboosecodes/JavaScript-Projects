4cd91dcdada944cf4e1eb2cdfdb9a00c9e4d52a4
caboosecodes <ricardolopez413@hotmail.com>
2021-05-13 09:47:57 -0700

    add methods toString, toPrecision, tofixed, valueOf

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -22,3 +22,25 @@ function where() {
     //because the result would replace the sentence with the index position of "you"
     document.getElementById("search").innerHTML = sen.search("you"); //returns 15
 }
+
+function numString() {
+    var x = 79;
+    document.getElementById("string").innerHTML = x.toString(); //displays 79 as a string
+    //toString() method can output numbers from base 2 to 32
+    //select the number base and place between the () in the method
+}
+
+function pFunction() {
+    var x = 21.258741258542584256985852;
+    document.getElementById("five").innerHTML = x.toPrecision(5); //rounds up
+}
+
+function places() {
+    var x = 5.24865258965;
+    document.getElementById("three").innerHTML = x.toFixed(3); //converts num to string also rounds up
+}
+
+function num() {
+    var x = 45687152;
+    document.getElementById("number").innerHTML = x.valueOf(); // returns a number as a number
+}
