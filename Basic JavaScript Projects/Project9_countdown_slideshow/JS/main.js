b85ae2154d8429bc16899f2ddc03e32cbc90472a
caboosecodes <ricardolopez413@hotmail.com>
2021-05-13 13:50:03 -0700

    slide show function

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -12,3 +12,33 @@ function countdown() {
     tick();
 }
 
+// SLIDESHOW SECTION
+
+var slideIndex = 1; //global variable
+showSlides(slideIndex);
+
+// next and previous button controls
+function plusSlides(n) {
+    showSlides(slideIndex += n); //increments the slideIndex variable by n
+}
+
+//image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slideIndex; i++) { //for statement; it loops through the block of code; if i is greaters than slide index then increment i
+        slides[i].style.display = "none"; //[] mean index of an array, so [2] is the INDEX position of 2
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block"; //makes it so the other images do not show up
+    dots[slideIndex-1].className += " active"; //makes it so the other dots do not have .active styling
+}
