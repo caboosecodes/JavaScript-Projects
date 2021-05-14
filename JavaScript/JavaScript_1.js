8a45e51577f68be2370f9436cbef84a62b945705
caboosecodes <ricardolopez413@hotmail.com>
2021-05-14 15:51:32 -0700

    use getElementByClassName()

--- JavaScript/JavaScript_1.js
+++ JavaScript/JavaScript_1.js
@@ -1,8 +1,8 @@
 function sidesFunction() {
     var output;
-    var sides = document.getElementById("sides").value
+    var sides = document.getElementById("sides").value //grabs the value from the input on the html file
     var sidesString = " is a great choice, enjoy your meal!"
-    switch(sides) {
+    switch(sides) { //switch statement
         case "Fries":
             output = "Fries" + sidesString;
         break;
@@ -18,8 +18,14 @@ function sidesFunction() {
         case "Caesar salad":
             output = "Caesar salad" + sidesString;
         break;
-        default:
-        output = "Please enter a side exactly as written in the list above."
+        default: 
+        output = "Please enter a side exactly as written in the list above." // if none of the input match the elements in the list then this will be displayed
     }
     document.getElementById("sidesOutput").innerHTML = output;
+}
+
+//getElementsByClassName()
+function elo() {
+    var x = document.getElementsByClassName("telephone");
+    x[0].innerHTML = "Hello, how are you?"; //[0] means that the string will displayed on the first element with the class "telephone"
 }
