94899deb24fa5f550257a176046302052499889f
caboosecodes <ricardolopez413@hotmail.com>
2021-05-15 12:46:49 -0700

    add canvas image and gradient

--- JavaScript/JavaScript_1.js
+++ JavaScript/JavaScript_1.js
@@ -29,3 +29,22 @@ function elo() {
     var x = document.getElementsByClassName("telephone");
     x[0].innerHTML = "Hello, how are you?"; //[0] means that the string will displayed on the first element with the class "telephone"
 }
+
+//canvas
+function hi() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d"); //creates "ConvasRenderingContext2D" object that represents a 2d rendering context
+    var img = document.getElementById("ralph");
+    ctx.drawImage(img, -20, 0); //img at x, y coordinates
+}
+
+//gradient
+var c = document.getElementById("grad"); 
+var ctx = c.getContext("2d");
+
+var grd = ctx.createLinearGradient(100, 150, 170, 200); //the gradient of the collor; first 2 numbers are the start point of the gradient (x,y), the last 2 nums. are the endpoint of the gradient.
+grd.addColorStop(0, "purple"); //starts purple
+grd.addColorStop(1, "red"); //ends red
+
+ctx.fillStyle = grd;
+ctx.fillRect(45, 81, 150, 100); //position within canvas
